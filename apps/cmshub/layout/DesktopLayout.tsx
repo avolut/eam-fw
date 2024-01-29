18916e27176e718509f241974edb3ce9ace707d0
rizrmd <me@rizrmd.com>
2024-01-29 01:10:05 -0700

    layout

--- apps/cmshub/layout/DesktopLayout.tsx
+++ apps/cmshub/layout/DesktopLayout.tsx
@@ -1,5 +1,35 @@
 import { FC, ReactNode } from "react";
+import { icon } from "@/comps/icon";
+
+const menus = {
+  admin: [
+    ["Dashboard", icon.home, "/dashboard"],
+    ["Structure", icon.inspect, "/structure"],
+    ["Content", icon.qr, "/content"]
+  ],
+  moderator: [
+    ["Dashboard", icon.home, "/dashboard"],
+    ["Content", icon.qr, "/content"]
+  ],
+};
 
 export const DesktopLayout: FC<{ children: ReactNode }> = ({ children }) => {
-  return <>Desktop</>;
+  return (
+    <div className={cx("c-flex c-w-full c-justify-between")}>
+      <div
+        className={cx(
+          "c-p-4 c-border",
+          css`
+            width: 300px;
+          `
+        )}
+      >
+        <div>{icon.cmshub}</div>
+      </div>
+      <div className={cx("c-border c-w-full")}>
+        <div>TopBar</div>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
 };
