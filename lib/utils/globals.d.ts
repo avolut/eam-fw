63576e8b3a4377d0c4dd2988b9fc00e10a7b5d08
rizrmd <me@rizrmd.com>
2024-02-05 06:20:05 -0700

    fix

--- lib/utils/globals.d.ts
+++
@@ -1,8 +0,0 @@
-declare var db: any;
-declare var isEditor: boolean;
-declare var isMobile: boolean;
-declare var isDesktop: boolean;
-declare var css: any;
-declare var cx: any;
-declare var preload: (urls: string[] | string) => any;
-declare var navigate: (link: string) => void;
